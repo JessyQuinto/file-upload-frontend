01309ff25e2e21da7f0a1f8f87fd7a09ce361919
JessyQT <60488881+JessyQuinto@users.noreply.github.com>
2024-08-12 10:10:33 -0500

    UPDATE

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  imports: [NavigationComponent]
+  imports: [NavigationComponent, RouterModule]
 })
 export class HeaderComponent { }
